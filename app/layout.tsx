a2403fc783806f33bce52865f101a6be890009e1
Mona Abishek A <kdcq009@astrazeneca.net>
2024-11-13 19:14:29 +0530

    updated styling files

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased`}
+        className={`${poppins.variable}`}
       >
         {children}
       </body>
